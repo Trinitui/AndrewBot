42a551dc1eeabd3f42accc0095971b352b891d76
Andrew W <andrewmwhelan9@gmail.com>
2020-08-12 19:57:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}! on server ${guild.name}`);
+    console.log(`Logged in as ${client.user.tag}! on server ${message.guild.name}`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
