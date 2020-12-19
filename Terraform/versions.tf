7d29e7af95588d16d42d215629f0bd39c98e0124
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2020-12-19 10:32:00 -0500

    Removing versions.

--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -1,21 +1,16 @@
 terraform {
-  required_version = ">= 0.12.26"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 2.0"
     }
     local = {
       source = "hashicorp/aws"
-      version = "~> 1.0"
     }
     null = {
       source = "hashicorp/null"
-      version = "~> 2.0"
     }
     template = {
-      source = "hashicorp/null"
-      version = "~> 2.0"
+      source = "hashicorp/template"
     }
   }
 }
