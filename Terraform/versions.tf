151f342ebe51ae1f2a55c64dba91c5dedfe75963
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2020-12-19 10:30:14 -0500

    Updating versions.

--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -1,11 +1,19 @@
 terraform {
-  required_version = ">= 0.12.0, < 0.14.0"
+  required_version = ">= 0.12.26"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "~> 2.0"
     }
+    local = {
+      source = "hashicorp/aws"
+      version = "~> 1.0"
+    }
     null = {
+      source = "hashicorp/null"
+      version = "~> 2.0"
+    }
+    template = {
       source = "hashicorp/null"
       version = "~> 2.0"
     }
