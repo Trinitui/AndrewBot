32566f579928d484d05b3a08ef4f4319354fbc07
awhelan <andrew.whelan@bioraft.com>
2020-12-19 09:50:03 -0500

    adding var and fixing nas

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -41,8 +41,8 @@ module "elastic_beanstalk_environment" {
   region                             = var.aws_region
   availability_zone_selector         = "Any 2"
   elastic_beanstalk_application_name = module.elastic_beanstalk_application.elastic_beanstalk_application_name
-  application_subnets = "n/a"
-  vpc_id = "n/a"
+  application_subnets = ""
+  vpc_id = ""
 
   instance_type           = "t3.small"
   autoscale_min           = 1
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   type    = string
   default = "us-east-2"
 }
+
+variable "namespace" {
+  type = string
+  default = "AndrewBot"
+}
