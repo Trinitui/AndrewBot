7ef2b82251cd1498614d8ca61f47a2babd2d5d51
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2020-12-19 14:12:40 -0500

    PR: Testing workspace (#22)
    * Updating TF & workflow.
    
    * Updating versions.
    
    * Removing versions.
    
    * Adding modules without variables.
    
    * Adding variables.
    
    * Disabling files for workspace creation.
    
    * Fixing working directory.
    
    * Fixing working directory.
    
    * Fixing working directory.
    
    * Enabling test.
    
    * Setting TF version.
    
    * Setting TF version.
    
    * Fixing provider.
    
    * Fixing errors.
    
    * Replacing use2 with use1.
    
    * Adding application version.
    
    * Fixing output.
    
    * Removing working directory.
    
    * Fixing version.
    
    * Fixing version.
    
    * Setting path to root.
    
    * Testing source file.
    
    * Using dir instead of file.
    
    * Using dir instead of file.
    
    * Using dir instead of file.
    
    * Running apply for testing.
    
    * Changing namespace.
    
    * Changing namespace.
    
    * Changing solution stack name.
    
    * Rolling back vars.
    
    * Updating main.

---
+++ Terraform/application_version.tf
@@ -0,0 +1,24 @@
+data "archive_file" "api_dist_zip" {
+  type        = "zip"
+  source_dir = "${path.cwd}/../AndrewBot"
+  output_path = "AndrewBot.zip"
+}
+
+resource "aws_s3_bucket" "dist_bucket" {
+  bucket = "${var.namespace}-elb-dist"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_object" "dist_item" {
+  key    = "${var.namespace}/dist-${uuid()}"
+  bucket = aws_s3_bucket.dist_bucket.id
+  source = data.archive_file.api_dist_zip.output_path
+}
+
+resource "aws_elastic_beanstalk_application_version" "default" {
+  name        = "AndrewBot-prod-${uuid()}"
+  application = module.elastic_beanstalk_application.elastic_beanstalk_application_name
+  description = "application version created by terraform"
+  bucket      = aws_s3_bucket.dist_bucket.id
+  key         = aws_s3_bucket_object.dist_item.id
+}
