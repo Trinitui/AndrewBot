1a9745cc800894c17fefee2ab4941641501bfecd
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2020-12-19 11:26:33 -0500

    Fixing version.

--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     archive = {
       source = "hashicorp/archive"
-      version = "= 2.0F"
+      version = "= 2.0"
     }
     aws = {
       source = "hashicorp/aws"
