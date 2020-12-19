91dba793bfa6eca1cd198756147370208d412f34
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2020-12-19 10:58:00 -0500

    Setting TF version.

--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -1,16 +1,21 @@
 terraform {
+  required_version = "= 0.13.5"
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "= 2.70"
     }
     local = {
       source = "hashicorp/aws"
+      version = "= 1.4"
     }
     null = {
       source = "hashicorp/null"
+      version = "= 2.1"
     }
     template = {
       source = "hashicorp/template"
+      version = "= 2.2"
     }
   }
 }
