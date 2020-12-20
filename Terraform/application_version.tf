4373059ceeab51327e54f83f09616de231b2357c
awhelan <andrew.whelan@bioraft.com>
2020-12-20 09:12:46 -0500

    changing vars to hardcode andrewbot ref

--- Terraform/application_version.tf
+++ Terraform/application_version.tf
@@ -5,18 +5,18 @@ data "archive_file" "api_dist_zip" {
 }
 
 resource "aws_s3_bucket" "dist_bucket" {
-  bucket = "${var.namespace}-elb-dist"
+  bucket = "andrewbot-elb-dist"
   acl    = "private"
 }
 
 resource "aws_s3_bucket_object" "dist_item" {
-  key    = "${var.namespace}/dist-${uuid()}"
+  key    = "andrewbot/dist-${uuid()}"
   bucket = aws_s3_bucket.dist_bucket.id
   source = data.archive_file.api_dist_zip.output_path
 }
 
 resource "aws_elastic_beanstalk_application_version" "default" {
-  name        = "AndrewBot-prod-${uuid()}"
+  name        = "andrewbot-prod-${uuid()}"
   application = module.elastic_beanstalk_application.elastic_beanstalk_application_name
   description = "application version created by terraform"
   bucket      = aws_s3_bucket.dist_bucket.id
